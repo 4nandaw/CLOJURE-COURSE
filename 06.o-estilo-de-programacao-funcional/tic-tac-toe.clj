42cf2276bff9fd8a0691bef054724612ae75c793
Ananda Vilar Vidal <ananda.vilar.vidal@ccc.ufcg.edu.br>
2024-04-22 14:58:12 -0300

    indexes 1-3

--- 06.o-estilo-de-programacao-funcional/tic-tac-toe.clj
+++ 06.o-estilo-de-programacao-funcional/tic-tac-toe.clj
@@ -2,7 +2,9 @@
   (vec (repeat 3 (vec (repeat 3 " ")))))
 
 (defn get-indexes [move]
-  (map #(rem (Integer/parseInt %) 10) move))
+  (let [row (Integer/parseInt (str (first move)))
+        col (Integer/parseInt (str (last move)))]
+    [(dec row) (dec col)]))
 
 (defn in-range? [num start end]
   (<= start num end))
@@ -27,7 +29,7 @@
 (defn victory [possibility last-player]
   (every? #(= % last-player) possibility))
 
-(defn is-board-full [board]
+(defn board-full? [board]
   (every? #(not= " " %) (apply concat board)))
 
 (defn get-elem [board indexes]
@@ -41,26 +43,22 @@
       (println "  ───┼───┼─── ")))
   (println "   "))
 
-(defn get-input [player]
-  (loop []
-    (let [move (clojure.string/trim (clojure.string/lower-case (read-line)))]
-      (cond
-        (= move "q!") (do (println "Exiting the game...") "q!")
-        (or (empty? move) (not= 2 (count move))) ""
-        :else (str (dec (Integer/parseInt (subs move 0 1))) (dec (Integer/parseInt (subs move 1 2))))))))
+(defn get-input [msg]
+  (println msg)
+  (read-line))
 
 (defn valid-move? [board indexes]
   (and (every? #(in-range? % 0 2) indexes)
        (= " " (get-in board indexes))))
 
-(defn validate-input [input]
+(defn valid-input? [input]
   (and (not (empty? input)) (= 2 (count input)) (numeric? input)))
 
 (defn make-move [board player move]
   (let [new-board (vec (map vec board))]
     (assoc-in new-board move player)))
 
-(defn winner [board player]
+(defn winner? [board player]
   (some #(victory % player) (get-all-victory-possibilities board)))
 
 (defn choose-next-player [current-player]
@@ -73,20 +71,14 @@
   (if victory
     (assoc scores player (inc (get scores player)))
     (if is-draw
-      (update scores "Draw" (fnil inc 0))
+      (assoc scores "Draw" (inc (get scores "Draw")))
       scores)))
 
 (defn print-winner [player]
-  (println (str "\nPlayer " player " is the winner")))
+  (println (str "\nPlayer " player " is the winner!")))
 
 (defn print-draw []
-  (println "\nNo one wins"))
-
-(defn print-invalid [msg]
-  (println (str "Invalid " msg "\n")))
-
-(defn print-exit []
-  (println "TYPE 'q!' TO QUIT AT ANY TIME\n"))
+  (println "\nNo one wins..."))
 
 (defn print-scores [scores]
   (println "--- Scores ---")
@@ -94,41 +86,44 @@
     (println (str player ": " score)))
   (println "--------------"))
 
-(defn wanna-play [victory is-draw]
+(defn wanna-play? [victory is-draw]
   (if (or victory is-draw)
     (loop []
-      (let [response (.toLowerCase (read-line))]
+      (let [response (.toLowerCase (get-input "Wanna play another game? (y/n)"))]
         (cond
           (= response "y") true
           (= response "n") false
-          :else (do (print-invalid "input. Please enter 'y' to play again or 'n' to quit.") (recur)))))
+          :else (do (println "Invalid input. Please enter 'y' to play again or 'n' to quit.") (recur)))))
     true))
 
-(defn main []
-  (let [board (create-board)
-        scores (atom {"X" 0, "O" 0, "Draw" 0})
-        player (atom "X")
-        playing (atom true)]
-    (print-exit)
-    (while @playing
-      (show-board board)
-      (let [move (get-input @player)]
-        (if (validate-input move)
-          (let [indexes (get-indexes move)]
-            (if (valid-move? board indexes)
-              (let [new-board (make-move board @player indexes)
-                    victory-status (winner new-board @player)
-                    is-draw (and (not victory-status) (is-board-full new-board))
-                    next-player (choose-next-player @player)
-                    scores-updated (update-scores victory-status is-draw @scores @player)]
-                (reset! board (choose-next-board victory-status is-draw new-board))
-                (when victory-status (show-board new-board) (print-winner @player))
-                (when is-draw (show-board new-board) (print-draw))
-                (reset! playing (wanna-play victory-status is-draw))
-                (reset! player next-player)
-                (reset! scores scores-updated))
-              (print-invalid "move. Try again")))
-          (when (= move "q!") (reset! playing false)))))
-  (print-scores scores)))
+(defn -main []
+  (loop [board (create-board)
+         player "X"
+         scores {"X" 0 "O" 0 "Draw" 0}
+         playing true]
+    (if playing
+      (do
+        (show-board board)
+        (let [move (vec (get-input (format "Move for player %s: " player)))]
+          (if (valid-input? move)
+            (let [indexes (get-indexes move)]
+             (if (valid-move? board indexes)
+              (let [new-board (make-move board player indexes)
+                    victory (winner? new-board player)
+                    is-draw (and (not victory) (board-full? new-board))
+                    next-player (choose-next-player player)
+                    next-board (choose-next-board victory is-draw new-board)
+                    next-scores (update-scores victory is-draw scores player)]
+                (cond
+                  victory (do (show-board new-board) (print-winner player))
+                  is-draw (do (show-board new-board) (print-draw)))
+                (recur next-board next-player next-scores (wanna-play? victory is-draw)))
+              (do
+                (print "Invalid move\n")
+                (recur board player scores playing))))
+            (do
+              (print "Invalid input\n")
+              (recur board player scores playing)))))
+      (print-scores scores))))
 
-(main)
+(-main)
